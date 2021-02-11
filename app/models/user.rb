80e74aecff421a75fd4e03490adffce3098115af
Ihar Halushka <galuropek@gmail.com>
2021-02-11 22:48:47 +0300

    Add email downcase transform before saving

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ActiveRecord::Base
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
             # uniqueness: true
+
+  before_save { self.email = email.downcase }
 end
