d1771ad38fcd01dccba758969d03e1711635bb95
Ihar Halushka <galuropek@gmail.com>
2021-02-12 21:22:01 +0300

    Use 'bang' method for defore_save logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
             uniqueness: { case_sensitive: false }
             # uniqueness: true
 
-  before_save { self.email = email.downcase }
+  before_save { self.email.downcase! }
 
   has_secure_password
 end
