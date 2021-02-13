d527a721ab66c2ce78ed47da39895fdadcdb93a6
Ihar Halushka <galuropek@gmail.com>
2021-02-13 13:23:41 +0300

    Add flash message for success registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      # todo - add success response
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
