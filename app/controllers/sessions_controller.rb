ca0ea186e15341f48d964d3af04e19fdf7db961d
Ihar Halushka <galuropek@gmail.com>
2021-02-19 20:47:59 +0300

    Add log_in functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,13 +6,15 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params.dig(:session, :email).downcase)
     if user && user.authenticate(params.dig(:session, :password))
-      # todo - log in
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = "Invalid email/password combination"
-      render 'new'
     end
   end
 
   def destroy
   end
 end
+
+render 'new'
