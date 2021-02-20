201cb26d9bdf4cb68d85ae30c804f289be50fdce
Ihar Halushka <galuropek@gmail.com>
2021-02-20 12:47:46 +0300

    Add test for vulnerable cases of Log out

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -19,6 +19,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count: 0
@@ -34,9 +35,4 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
-
-  # test "login with valid information followed by logout" do
-  #   get login_path
-  #   post login_path, session: { email: @user.email, password: "password" }
-  # end
 end
