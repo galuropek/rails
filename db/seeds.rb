0a4e6d52a317b567974e452454e0671b293bf61a
Ihar <galuropek@gmail.com>
2021-02-25 22:37:05 +0300

    Protect destroy method

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,11 @@
 User.create!(name: "Example user",
              email: "example@gmail.com",
              password: "foobar",
-             password_confirmation: "foobar")
+             password_confirmation: "foobar",
+             admin: true)
 99.times do |n|
   name = Faker::Name.name
-  email = "example-#{n+1}@gmail.com"
+  email = "example-#{n + 1}@gmail.com"
   password = "password"
   User.create!(name: name,
                email: email,
