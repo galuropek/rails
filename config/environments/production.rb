7ad174fbab23610660ce2e28f9a627bb0dd34bb4
Ihar <galuropek@gmail.com>
2021-03-13 15:21:34 +0300

    Smtp edit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,12 +66,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'murmuring-plains-56063.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :address => 'smtp.sendgrid.net',
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp.gmail.com',
     :port => '587',
-    :authentication => :plain,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
+    :authentication => "plain",
+    :user_name => ENV['GMAIL_USERNAME'],
+    :password => ENV['GMAIL_PASSWORD'],
     :domain => 'heroku.com',
     :enable_starttls_auto => true
   }
