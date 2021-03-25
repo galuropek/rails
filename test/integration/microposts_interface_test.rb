a12d9edc5dc1211c20405dcc3b15c9f6fbdd6aa9
Ihar <galuropek@gmail.com>
2021-03-25 20:52:53 +0300

    Add micropost interface integration test

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -20,13 +20,14 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_difference 'Micropost.all.count', 1 do
       post microposts_path, micropost: { content: content }
     end
-    assert_redirected_to root_url follow_redirect!
+    assert_redirected_to root_url
+    follow_redirect!
     assert_match content, response.body
     # remove micropost
     assert_select 'a', text: 'delete'
     first_micropost = @user.microposts.paginate(page: 1).first
     assert_difference 'Micropost.all.count', -1 do
-      delete microposts_path(first_micropost)
+      delete micropost_path(first_micropost)
     end
     # move to other user
     get user_path(users(:test_user_2))
