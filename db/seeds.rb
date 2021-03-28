d3e9fee7d556c81ac54d860032a0ba529f6484e1
Ihar <galuropek@gmail.com>
2021-03-28 13:57:24 +0300

    Add relationships to db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# Users
+
 User.create!(name: "Example user",
              email: "example@gmail.com",
              password: "foobar",
@@ -18,8 +20,19 @@ User.create!(name: "Example user",
                activated_at: Time.zone.now)
 end
 
+# Microposts
+
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+# Relationships
+
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
